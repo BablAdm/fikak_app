61d92105677c60ddcef0bb5aedc3426dc06692a8
amine bourouffala <mbourouffala@aas.com.sa>
2024-10-26 18:44:47 +0000

    feat: new enhancement on the login page

--- fikak_app/hooks.py
+++ fikak_app/hooks.py
@@ -154,9 +154,6 @@ app_license = "MIT"
 # ------------------------------
 #
 
-override_whitelisted_methods = {
-	"api.handler.nafath_callback": "fikak_app.fikak_api.integrations_api.nafath_callback",
-}
 #
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
