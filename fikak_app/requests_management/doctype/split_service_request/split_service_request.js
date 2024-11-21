d300a9ffde68ac3b50d8c0b9c2072b843f049524
bourouffala <m.elamine@waseera.sa>
2024-11-21 17:42:52 +0000

    feat: develop requests doctypes (evaluation , split service)

---
+++ fikak_app/requests_management/doctype/split_service_request/split_service_request.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Waseera and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Split Service Request", {
+// 	refresh(frm) {
+
+// 	},
+// });
