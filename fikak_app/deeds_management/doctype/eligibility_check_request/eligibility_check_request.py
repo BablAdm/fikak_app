1ac8e4f3a77b91dfa9ab86a0000ff761524d2770
zoubir <zizou_medj@hotmail.com>
2024-12-06 07:40:19 +0000

    Implement Deed History actions ( Elgibility )

--- fikak_app/deeds_management/doctype/eligibility_check_request/eligibility_check_request.py
+++ fikak_app/deeds_management/doctype/eligibility_check_request/eligibility_check_request.py
@@ -1,7 +1,8 @@
 # Copyright (c) 2024, Waseera and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.utils import now
 from frappe.model.document import Document
 
 
@@ -13,18 +14,66 @@ class EligibilityCheckRequest(Document):
 		pass
 
 	def on_update(self):
-		
-		if not self._doc_before_save:
-			#TODO : Update all new deeds
-			for deed in self.requested_deeds:
-				update_deed_status(deed.deed , "NEW")
-			pass
-		else:
-			#TODO : Update only the new deeds
-			#TODO : Update deeds that are not in the old request and the status has been changed
-			pass
-		pass
+
+		# Iterate over child table items and detect status changes
+		for item in self.get("requested_deeds"):
+
+			# Fetch the previous state of the document before the current save
+			previous_doc = self.get_doc_before_save()
+
+			# Iterate over child table items to compare statuses
+			for item in self.get("requested_deeds"):
+				# Find the corresponding item in the previous state
+				previous_item = (
+					next((prev for prev in previous_doc.get("requested_deeds") if prev.name == item.name), None)
+					if previous_doc else None
+				)
+			# If the item is new or the status has changed, update the Deed Doctype
+			if previous_item is None or previous_item.status != item.status:
+				service = "Eligibility Check"
+				status = item.get("status")
+				if(status == "NEW"):
+					status = "Pending"
+				update_deed_workflow(item.deed,service, status)
+
+			# # Get the old status from the cached statuses
+			# old_status = self._cached_statuses.get(item.name)
+
+			# # Check if the status has changed
+			# if old_status != item.status:
+			# 	service = "Eligibility Check"
+			# 	status = item.get("status")
+			# 	if(status == "NEW"):
+			# 		status = "Pending"
+			# 	update_deed_workflow(item.deed,service, status)
+	
 
 
-def update_deed_status(deed_id , status):
-	pass
+def update_deed_workflow(deed_name ,service, status):
+    """
+    Update the 'Deed' Doctype based on changes in 'Eligibility Check Request Deed Item'
+    """	
+	## 1. get deed doctype
+    deed_dt = frappe.get_doc("WATHEQ Deed", deed_name)
+
+    if not deed_dt:
+        frappe.throw(f"Deed with name '{deed_name}' not found.")
+
+    # 2.Update the Deed status and services
+    deed_dt.service_status = status
+    deed_dt.service = service
+
+    # 3. Add a new entry to the Watheq Deed Status Item table
+    deed_dt.append("statuses", {
+        "submission_date": now(),
+        "service": service,
+        "status": status
+    })
+
+    # Save the changes to the Deed Doctype
+    deed_dt.save(ignore_permissions=True)
+
+
+
+
+ 
