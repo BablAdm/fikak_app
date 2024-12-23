5c12e622bf87800f4dcda1ceaac4f1244f3e2502
zoubir <zizou_medj@hotmail.com>
2024-12-23 15:01:55 +0000

    add down price on the pricing block

--- fikak_app/fikak_api/mode_test_api.py
+++ fikak_app/fikak_api/mode_test_api.py
@@ -91,7 +91,7 @@ def get_eligibility_request_deed_data(deed_id, request_id=0):
             "total_interest_payment": deed.total_interest_payment,
             "total_principal_payment": deed.total_principal_payment,
             "deed_price": deed_dt.deed_price,
-            "deed_price": deed_dt.deed_price,
+            "down_price": deed_dt.down_price,
             "interest_amount": deed_dt.interest_amount,
         }
 
